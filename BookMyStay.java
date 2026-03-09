74bef06eea8d59cc9cd73455ad03d66a671e1f0a
PraaneethReddy24 <lpraaneethr@gmail.com>
2026-03-09 09:04:43 +0530

    Base application setup

---
+++ BookMyStay.java
@@ -0,0 +1,20 @@
+/**
+ * HotelBookingApp
+ *
+ * This class serves as the entry point for the Hotel Booking application.
+ * It demonstrates how a Java program begins execution and produces console output.
+ * The program prints a welcome message along with the application name and version.
+ *
+ * @author YourName
+ * @version 1.0
+ */
+public class HotelBookingApp {
+    public static void main(String[] args) {
+        String appName = "Hotel Booking Application";
+        String appVersion = "v1.0";
+
+        System.out.println("Welcome to " + appName + "!");
+        System.out.println("Application Version: " + appVersion);
+        System.out.println("Thank you for using our service.");
+    }
+}
