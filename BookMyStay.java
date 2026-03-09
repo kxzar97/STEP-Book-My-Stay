23a31da14738fa15c5bea006d1001df7b56c503d
kxzar97 <kxzarx97@gmail.com>
2026-03-09 10:39:07 +0530

    UC6: Reservation Confirmation & Room Allocation

--- BookMyStay.java
+++ BookMyStay.java
@@ -1,15 +1,20 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BookMyStay {
 
     public static void main(String[] args) {
 
-        BookingRequestQueue queue = new BookingRequestQueue();
+        RoomInventory inventory = new RoomInventory();
+        InventoryService inventoryService = new InventoryService(inventory);
+        BookingService bookingService = new BookingService(inventoryService);
 
-        // Guest booking requests
-        queue.addRequest(new Reservation("Abhi", "Single"));
-        queue.addRequest(new Reservation("Subha", "Double"));
-        queue.addRequest(new Reservation("Vanmathi", "Suite"));
+        Queue<Reservation> bookingQueue = new LinkedList<>();
 
-        // Process queue
-        queue.processRequests();
+        bookingQueue.add(new Reservation("Abhi", "Single"));
+        bookingQueue.add(new Reservation("Subha", "Double"));
+        bookingQueue.add(new Reservation("Vanmathi", "Suite"));
+
+        bookingService.processBookings(bookingQueue);
     }
 }
--- RoomInventory.java
+++ RoomInventory.java
@@ -5,14 +5,21 @@ public class RoomInventory {
     private HashMap<String, Integer> inventory;
 
     public RoomInventory() {
+
         inventory = new HashMap<>();
 
-        inventory.put("Single Room", 5);
-        inventory.put("Double Room", 3);
-        inventory.put("Suite Room", 2);
+        inventory.put("Single", 5);
+        inventory.put("Double", 3);
+        inventory.put("Suite", 2);
     }
 
     public int getAvailability(String roomType) {
         return inventory.getOrDefault(roomType, 0);
+    }
+
+    public void updateAvailability(String roomType, int change) {
+
+        int current = inventory.get(roomType);
+        inventory.put(roomType, current + change);
     }
 }
